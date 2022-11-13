c38b0de28f21922576afdddee62ad1386f3cb43c
Jonathan Ludvigsson <114058170+JonathanLudvigsson@users.noreply.github.com>
2022-11-13 19:19:21 +0100

    a

--- LabbPolymorphism/Geometry.cs
+++ LabbPolymorphism/Geometry.cs
@@ -28,7 +28,6 @@ namespace LabbPolymorphism
                 Console.WriteLine($"{shape.Name} Area: {shape.Area()}\n{shape.Name} Omkrets: {shape.Circumference()}");
                 Console.WriteLine();
             }
-
             Console.WriteLine("Vill du testa att räkna ut omkrets eller area med dina egna värden?");
             if (Console.ReadLine().ToLower() == "ja")
             {
@@ -42,35 +41,41 @@ namespace LabbPolymorphism
             {
                 Console.Clear();
                 Console.WriteLine("Hej! Vilken form vill du räkna ut omkrets eller area på? \nDu kan välja mellan Cirkel, Fyrkant eller Rektangel");
-                string shape = Console.ReadLine().ToLower();
 
                 Geometry g;
-                switch (shape)
+                bool error;
+                do
                 {
-                    case "cirkel":
-                        Console.WriteLine("Skriv in radien på din cirkel i centimeter");
-                        g = new Circle(Double.Parse(Console.ReadLine()));
-                        Console.WriteLine($"Area: {g.Area()}\nOmkrets: {g.Circumference()}");
-                        Console.ReadKey();
-                        break;
-                    case "fyrkant":
-                        Console.WriteLine("Skriv in längden på sidan av din fyrkant");
-                        g = new Square(Double.Parse(Console.ReadLine()));
-                        Console.WriteLine($"Area: {g.Area()}\nOmkrets: {g.Circumference()}");
-                        Console.ReadKey();
-                        break;
-                    case "rektangel":
-                        Console.WriteLine("Skriv in bredden och sedan höjden av din rektangel");
-                        g = new Rectangle(Double.Parse(Console.ReadLine()), Double.Parse(Console.ReadLine()));
-                        Console.WriteLine($"Area: {g.Area()}\nOmkrets: {g.Circumference()}");
-                        Console.ReadKey();
-                        break;
-                    default:
-                        Console.WriteLine("Skriv in en giltig form");
-                        Console.ReadKey();
-                        Console.Clear();
-                        break;
-                }
+                    error = false;
+                    switch (Console.ReadLine().ToLower())
+                    {
+                        case "cirkel":
+                            Console.WriteLine("Skriv in radien på din cirkel");
+                            g = new Circle(Double.Parse(Console.ReadLine()));
+                            Console.WriteLine($"Area: {g.Area()}\nOmkrets: {g.Circumference()}");
+                            Console.ReadKey(true);
+                            break;
+                        case "fyrkant":
+                            Console.WriteLine("Skriv in längden på sidan av din fyrkant");
+                            g = new Square(Double.Parse(Console.ReadLine()));
+                            Console.WriteLine($"Area: {g.Area()}\nOmkrets: {g.Circumference()}");
+                            Console.ReadKey(true);
+                            break;
+                        case "rektangel":
+                            Console.WriteLine("Skriv in bredden och sedan höjden av din rektangel");
+                            g = new Rectangle(Double.Parse(Console.ReadLine()), Double.Parse(Console.ReadLine()));
+                            Console.WriteLine($"Area: {g.Area()}\nOmkrets: {g.Circumference()}");
+                            Console.ReadKey(true);
+                            break;
+                        default:
+                            Console.WriteLine("Skriv in en giltig form");
+                            Console.ReadKey(true);
+                            Console.Clear();
+                            error = true;
+                            break;
+                    }
+                } while (error != true);
+
             }
         }
     }
